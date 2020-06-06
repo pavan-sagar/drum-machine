4323c01edf53968a1aa3dacbb91ea8d39ceb7ff5
pavan-sagar <pavan.sagar@outlook.com>
2020-06-06 15:01:10 +0530

    Updates

---
+++ precache-manifest.3adc8821c348f905b9a5fe927b4b6acb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd180dbdf3b9a4bcf6d01560fcfc57e4",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "39f2f4f149fc6e568cc7",
+    "url": "/drum-machine/static/css/main.102050bd.chunk.css"
+  },
+  {
+    "revision": "83c5452dc2ed526ee36d",
+    "url": "/drum-machine/static/js/2.96cbdf93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.96cbdf93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f2f4f149fc6e568cc7",
+    "url": "/drum-machine/static/js/main.d810a463.chunk.js"
+  },
+  {
+    "revision": "fc1b3e84514a8c25bf54",
+    "url": "/drum-machine/static/js/runtime-main.1e07d996.js"
+  },
+  {
+    "revision": "1850b2efc197cfaae3781d8cb67b2dcf",
+    "url": "/drum-machine/static/media/DISKOTEQUE.1850b2ef.TTF"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
